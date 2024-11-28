b745e60f2bac9fe06e7592d72fc9018d19567d0c
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-11-28 12:33:18 +0700

    Push perubahan

--- Percobaan2.java
+++ Percobaan2.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Percobaan2 {
     
+    static String tanda = "";
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
+            tanda += "1";
             return (1);
         } else {
+            tanda += x + "x";
             return (x * hitungPangkat(x, y - 1));
         }
     }
@@ -18,6 +21,8 @@ public class Percobaan2 {
         System.out.print("Pangkat: ");
         int pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        tanda = "";
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println(tanda + " = " + hasil);
     }
 }
